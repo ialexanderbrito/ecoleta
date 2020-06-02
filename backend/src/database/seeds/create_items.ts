92f39ea92fb9543d8266850c7d18cd0fd0656876
ialexanderbrito <xanderzinho26@gmail.com>
2020-06-02 13:35:06 -0300

    Criado banco de dados com Knex

---
+++ backend/src/database/seeds/create_items.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex) {
+  await knex('items').insert([
+    { title: 'Lâmpadas', image: 'lampadas.svg' },
+    { title: 'Pilhas e Baterias', image: 'baterias.svg' },
+    { title: 'Papéis e Pepelão', image: 'papeis-papelao.svg' },
+    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+    { title: 'Redíduos Orgânicos', image: 'organicos.svg' },
+    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
+  ]);
+}
