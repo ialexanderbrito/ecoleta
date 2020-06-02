0f5106e03074c2fd1c0cb7c7780ad4ec4ab1168a
ialexanderbrito <xanderzinho26@gmail.com>
2020-06-02 14:54:44 -0300

    Adicionado CORS e alteração de rotas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 
-const routes = express.Router();
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Oi' });
-});
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,14 @@
-import express, { response } from 'express';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
