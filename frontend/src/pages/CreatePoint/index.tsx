f6fe8cf03704cc5482a0289cb64031c836278e60
ialexanderbrito <xanderzinho26@gmail.com>
2020-06-03 14:37:48 -0300

    Integração do mapa

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -52,6 +53,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço do mapa</span>
           </legend>
+
+          <Map center={[-22.7679807, -43.3352482]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-22.7679807, -43.3352482]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
