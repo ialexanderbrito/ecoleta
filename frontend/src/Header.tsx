bfa6ed6b030cbc0dbf40e0c6562f7c8e86baa4b0
ialexanderbrito <xanderzinho26@gmail.com>
2020-06-03 12:09:06 -0300

    Ordenação das pastas

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  );
+};
+
+export default Header;
