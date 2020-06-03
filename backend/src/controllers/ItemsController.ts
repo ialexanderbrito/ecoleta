0df118101628ea98b409a20a2ee2b1a00230d920
ialexanderbrito <xanderzinho26@gmail.com>
2020-06-03 15:01:13 -0300

    Alteração de name para title

--- backend/src/controllers/ItemsController.ts
+++ backend/src/controllers/ItemsController.ts
@@ -8,7 +8,7 @@ class ItemsController {
     const serializedItems = items.map((item) => {
       return {
         id: item.id,
-        name: item.title,
+        title: item.title,
         image_url: `http://localhost:3333/uploads/${item.image}`,
       };
     });
