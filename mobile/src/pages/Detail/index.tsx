993848ec4a8c0cb2f5fad2fe153517122e6d239c
Alexander <xanderzinho26@gmail.com>
2020-06-04 18:51:10 -0300

    Finalizado página do detalhe

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, TouchableOpacity, Image, Text } from 'react-native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
 
 const Detail = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image
+          style={styles.pointImage}
+          source={{
+            uri:
+              'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+          }}
+        />
+
+        <Text style={styles.pointName}>Mercadinho</Text>
+        <Text style={styles.pointItems}>Lâmpada, Óleo de Cozinha</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Praça Manuel Gonçalo, RJ</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={20} color="#fff" />
+          <Text style={styles.buttonText}>WhatsApp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" size={20} color="#fff" />
+          <Text style={styles.buttonText}>Email</Text>
+        </RectButton>
+      </View>
+    </>
+  );
 };
 
 export default Detail;
