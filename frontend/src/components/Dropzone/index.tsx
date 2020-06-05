7caa4ed492b66aa586045bcec98c5a9f5eda4bcc
Alexander <xanderzinho26@gmail.com>
2020-06-05 16:08:25 -0300

    Finalizado Dropzone

--- frontend/src/components/Dropzone/index.tsx
+++ frontend/src/components/Dropzone/index.tsx
@@ -4,16 +4,24 @@ import { FiUpload } from 'react-icons/fi';
 
 import './styles.css';
 
-const Dropzone = () => {
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
   const [selectedFileUrl, setSelectedFileUrl] = useState('');
 
-  const onDrop = useCallback((acceptedFiles) => {
-    const file = acceptedFiles[0];
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
 
-    const fileUrl = URL.createObjectURL(file);
+      const fileUrl = URL.createObjectURL(file);
 
-    setSelectedFileUrl(fileUrl);
-  }, []);
+      setSelectedFileUrl(fileUrl);
+      onFileUploaded(file);
+    },
+    [onFileUploaded]
+  );
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: 'image/*',
--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -50,6 +50,7 @@ const CreatePoint = () => {
     0,
     0,
   ]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -140,16 +141,20 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
 
     try {
       await api.post('points', data);
@@ -195,7 +200,7 @@ const CreatePoint = () => {
           <br /> ponto de coleta
         </h1>
 
-        <Dropzone />
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
