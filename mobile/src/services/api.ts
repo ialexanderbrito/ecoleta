2e1d3410a7239b9f428eed53a44656fdc2a7d951
Alexander <xanderzinho26@gmail.com>
2020-06-06 16:28:52 -0300

    Adicionado novo icone e splash

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from '../../.env.json';
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.4:3333',
+  baseURL: API_URL,
 });
 
 export default api;
