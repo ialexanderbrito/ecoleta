fbde95e824c93dfc717eb693a37797b3d99f1726
Alexander <xanderzinho26@gmail.com>
2022-10-24 11:30:25 -0300

    fix: link api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ecoleta-oficial.herokuapp.com',
+  baseURL: 'https://ecoleta-api.ialexanderbrito.dev',
 });
 
 export default api;
